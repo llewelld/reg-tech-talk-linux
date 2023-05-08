67d6a376ae75737c591faed2c3578f45ba24fe39
David Llewellyn-Jones <david@flypig.co.uk>
2023-05-08 16:51:51 +0100

    Shorten presentation
    Removes a couple of slides and moves a bunch of slides to the end as
    overflow.
    
    Also adds in some example scripts.

---
+++ examples/example.py
@@ -0,0 +1,61 @@
+#!/bin/python3
+
+# See: https://lightning.ai/docs/pytorch/stable/starter/introduction.html
+
+import os
+from torch import optim, nn, utils, Tensor
+from torchvision.datasets import MNIST
+from torchvision.transforms import ToTensor
+import lightning.pytorch as pl
+
+# define any number of nn.Modules (or use your current ones)
+encoder = nn.Sequential(nn.Linear(28 * 28, 64), nn.ReLU(), nn.Linear(64, 3))
+decoder = nn.Sequential(nn.Linear(3, 64), nn.ReLU(), nn.Linear(64, 28 * 28))
+
+# define the LightningModule
+class LitAutoEncoder(pl.LightningModule):
+    def __init__(self, encoder, decoder):
+        super().__init__()
+        self.encoder = encoder
+        self.decoder = decoder
+
+    def training_step(self, batch, batch_idx):
+        # training_step defines the train loop.
+        # it is independent of forward
+        x, y = batch
+        x = x.view(x.size(0), -1)
+        z = self.encoder(x)
+        x_hat = self.decoder(z)
+        loss = nn.functional.mse_loss(x_hat, x)
+        # Logging to TensorBoard (if installed) by default
+        self.log("train_loss", loss)
+        return loss
+
+    def configure_optimizers(self):
+        optimizer = optim.Adam(self.parameters(), lr=1e-3)
+        return optimizer
+
+# init the autoencoder
+autoencoder = LitAutoEncoder(encoder, decoder)
+
+# setup data
+dataset = MNIST(os.getcwd(), download=True, transform=ToTensor())
+train_loader = utils.data.DataLoader(dataset, num_workers=4)
+
+# train the model (hint: here are some helpful Trainer arguments for rapid idea iteration)
+trainer = pl.Trainer(limit_train_batches=100, max_epochs=1)
+trainer.fit(model=autoencoder, train_dataloaders=train_loader)
+
+# load checkpoint
+checkpoint = "./lightning_logs/version_0/checkpoints/epoch=0-step=100.ckpt"
+autoencoder = LitAutoEncoder.load_from_checkpoint(checkpoint, encoder=encoder, decoder=decoder)
+
+# choose your trained nn.Module
+encoder = autoencoder.encoder
+encoder.eval()
+
+# embed 4 fake images!
+fake_image_batch = Tensor(4, 28 * 28)
+embeddings = encoder(fake_image_batch)
+print("⚡" * 20, "\nPredictions (4 image embeddings):\n", embeddings, "\n", "⚡" * 20)
+
